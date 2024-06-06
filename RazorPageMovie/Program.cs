d823e24b35ce29d9d717cdf248f5586056eeea33
Prashant <prashantchapagain@gmail.com>
2024-06-06 13:47:43 +0545

    Added Movie Model

--- RazorPageMovie/Program.cs
+++ RazorPageMovie/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPageMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPageMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPageMovieContext' not found.")));
 
 var app = builder.Build();
 
